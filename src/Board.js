10db315f1d74997b98fca0251ce4c16d2dcd4e6c
AQP20171679 <alejandraadriantejada@gmail.com>
2017-11-07 10:59:35 -0500

    box

---
+++ src/Board.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './whale.png';
+import './App.css';
+
+const Board = ({}) => {
+  return (
+    <h1>Board</h1>
+  );
+}
+
+export default Board;

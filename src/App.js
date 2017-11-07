08acbf10858134b72a250f94e50d8ce4729326fa
AQP20171679 <alejandraadriantejada@gmail.com>
2017-11-07 11:13:06 -0500

    Inicio con sing in y sing up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './whale.png';
 import './App.css';
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, Redirect,NavLink} from 'react-router-dom'
 import SingUp from './SingUp.js'
 import SingIn from './SingIn.js'
 import Board from './Board.js'
+
+const Sing = ({}) => {
+  return (
+          <div className='text-center'>
+            <img className='logo-big' src={logo}/>
+            <NavLink to="/sing-in" className='sing-in' >Sing In</NavLink>
+            <NavLink to="/sing-up" className='sing-in' >Sing Up</NavLink>
+          </div>
+  );
+}
 
 const App = ({}) => {
   return (
@@ -12,6 +22,7 @@ const App = ({}) => {
       <BrowserRouter>
         <div>
           <Switch>
+            <Route exact path='/' component={Sing}/>
             <Route exact path="/sing-up" render={() =><SingUp/>}/>
             <Route exact path="/sing-in" render={() =><SingIn/>}/>
             <Route exact path="/board" render={() =><Board/>}/>

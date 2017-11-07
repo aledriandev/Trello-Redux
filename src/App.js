29e9f847095d1c06ed28df9651a0f87b050a0b56
AQP20171679 <alejandraadriantejada@gmail.com>
2017-11-07 10:59:07 -0500

    Ruteo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import SingUp from './SingUp.js'
+import SingIn from './SingIn.js'
+import Board from './Board.js'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = ({}) => {
+  return (
+    <div className='body'>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/sing-up" render={() =><SingUp/>}/>
+            <Route exact path="/sing-in" render={() =><SingIn/>}/>
+            <Route exact path="/board" render={() =><Board/>}/>
+            <Route render={() => <Redirect to="/" />} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

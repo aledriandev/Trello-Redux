d7f87cd2dfa9c85eac5ef618959c2f493aef0f30
AQP20171679 <alejandraadriantejada@gmail.com>
2017-11-08 06:26:31 -0500

    estatico users

--- src/index.js
+++ src/index.js
@@ -4,5 +4,184 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const users = [
+  {
+    name: 'Alejandra',
+    last: 'Adrian',
+    email: 'ale@gmail.com',
+    pasword: '123',
+    teams: [
+      {
+        name: 'Personal Boards',
+        members: ['user'],
+        boards: [
+          {
+            name: 'Board 1',
+            lists: [
+              {
+                name: 'List 1',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 2',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 3',
+                cards: ['card 1', 'card 2', 'card 3']
+              }
+            ]
+          },
+          {
+            name: 'Board 2',
+            lists: [
+              {
+                name: 'List 1',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 2',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 3',
+                cards: ['card 1', 'card 2', 'card 3']
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'States',
+        members: ['user'],
+        boards: [
+          {
+            name: 'Board 1',
+            lists: [
+              {
+                name: 'List 1',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 2',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 3',
+                cards: ['card 1', 'card 2', 'card 3']
+              }
+            ]
+          },
+          {
+            name: 'Board 2',
+            lists: [
+              {
+                name: 'List 1',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 2',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 3',
+                cards: ['card 1', 'card 2', 'card 3']
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Laura',
+    last: 'Garcia',
+    email: 'lau@gmail.com',
+    pasword: '123',
+    teams: [
+      {
+        name: 'Personal Boards',
+        members: ['user'],
+        boards: [
+          {
+            name: 'Board 1',
+            lists: [
+              {
+                name: 'List 1',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 2',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 3',
+                cards: ['card 1', 'card 2', 'card 3']
+              }
+            ]
+          },
+          {
+            name: 'Board 2',
+            lists: [
+              {
+                name: 'List 1',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 2',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 3',
+                cards: ['card 1', 'card 2', 'card 3']
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'States',
+        members: 'user',
+        boards: [
+          {
+            name: 'Board 1',
+            lists: [
+              {
+                name: 'List 1',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 2',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 3',
+                cards: ['card 1', 'card 2', 'card 3']
+              }
+            ]
+          },
+          {
+            name: 'Board 2',
+            lists: [
+              {
+                name: 'List 1',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 2',
+                cards: ['card 1', 'card 2', 'card 3']
+              },
+              {
+                name: 'List 3',
+                cards: ['card 1', 'card 2', 'card 3']
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+]
+
+ReactDOM.render(<App users={users} />, document.getElementById('root'));
 registerServiceWorker();
